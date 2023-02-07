8a6efd6990b0c3fe6c5421df1721420a427165a6
alannguyencs <alannguyen.cs@gmail.com>
2023-02-07 23:58:01 +0800

    update constants

--- hoaquason_crypto/constants.py
+++ hoaquason_crypto/constants.py
@@ -7,5 +7,8 @@ LOCAL_PATH = "/home/alan/GoogleDrive/investment/hoaquason_crypto/hoaquason_crypt
 IS_LOCAL = os.path.isdir(LOCAL_PATH)
 DRIVE_PATH = str(CWF.parent) + '/' if IS_LOCAL \
                     else '/content/drive/MyDrive/investment/hoaquason_crypto/hoaquason_crypto/'
+if not os.path.isdir(DRIVE_PATH):
+    DRIVE_PATH = "C:/Users/Alan/Documents/hoaquason_crypto/hoaquason_crypto/"
 print ('DRIVE_PATH:', DRIVE_PATH)
 BINANCE_LISTING_PATH = DRIVE_PATH + 'binance_listing.json'
+
